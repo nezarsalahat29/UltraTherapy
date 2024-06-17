bf84686f8652ea9dc663244e4563456673147e57
Nezar Al-Salahat <nezarsalahat29@gmail.com>
2024-06-17 14:03:16 +0300

    initialize

---
+++ UltraTherapy/src/main/java/com/ultra/UltraTherapy/UltraTherapyApplication.java
@@ -0,0 +1,13 @@
+package com.ultra.UltraTherapy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltraTherapyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UltraTherapyApplication.class, args);
+	}
+
+}
---
+++ UltraTherapy/src/test/java/com/ultra/UltraTherapy/UltraTherapyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ultra.UltraTherapy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltraTherapyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
